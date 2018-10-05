c2d7ebf106b7bf7e4ccbe98aef22c3f7b6039339
Abathargh <gim.marcello@gmail.com>
2018-10-05 11:45:50 +0200

    Corrected a small error, where a log that shouldn't be used was.

--- moody/utility/plotting.py
+++ moody/utility/plotting.py
@@ -47,7 +47,7 @@ def plot ( data_list, audio_types, audio_format ) :
         types = np.array ( types )
         plt.xticks( chunks_beg, types )       
         amplitude = amplitude /  np.iinfo( numpy_format ).max
-        plt.plot( 20 * np.log10( abs ( amplitude ) ) )
+        plt.plot ( amplitude )
         pathlib.Path ( "./moody/graphs/" ).mkdir ( parents = True, exist_ok = True ) 
         now = datetime.datetime.now()
         formatted_date = "{}_{}_{}-{}_{}_{}".format( now.day, now.month, now.year, now.hour, now.minute, now.second )        
